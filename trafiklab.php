001be7a09e9f268c1f1b5d872f3e88ca2b539942
Jakob Florell <jakob.florell@valtech.se>
2014-06-24 15:51:47 +0200

    Small fixes after first user testing sessions

--- trafiklab.php
+++ trafiklab.php
@@ -8,11 +8,13 @@
 	if ($_GET ['api'] == "platsuppslag") {
 		$url = "http://api.sl.se/api2/typeahead.json?key=701a00aed8e3482dab74149d93d7788a&stationsonly=true&maxresults=1&searchstring=".urlencode($_GET['q']);
 
-		echo  $cache->get_data($_GET['api'].'_'.$_GET['q'], $url);
+		//echo  $cache->get_data($_GET['api'].'_'.$_GET['q'], $url);
+		echo  $cache->do_curl($url);
 	} else if ($_GET ['api'] == "realtidsinformation") {
 		$url = "api.sl.se/api2/realtimedepartures.json?key=f2269025452a4fdebe1d0a119e006c8a&timewindow=15&siteid=".urlencode($_GET['q']);
 		
-		echo  $cache->get_data($_GET['api'].'_'.$_GET['q'], $url);
+		//echo  $cache->get_data($_GET['api'].'_'.$_GET['q'], $url);
+		echo  $cache->do_curl($url);
 	}
 
 ?>
